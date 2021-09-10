208a392f1380e2cff58266ff01267ab0459b3279
Yasuko <89954618+Yasmin027@users.noreply.github.com>
2021-09-11 00:48:04 +0900

    Create continue_break.js

---
+++ continue_break.js
@@ -0,0 +1,12 @@
+'use strict';
+
+for(let i = 1; i <=10; i++) {
+  // if(i === 4){         //iが4の時スキップする
+  // if(i % 3 === 0) {      //iが3の倍数の時スキップする
+  //   continue;      
+  // }
+  if(i === 4) {       //iが4になったら終了
+    break;
+  }
+  console.log(i);
+}
