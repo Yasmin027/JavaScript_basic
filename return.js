bbebcf9480b3e32dbcec895eebeea2671ce3b763
Yasuko <89954618+Yasmin027@users.noreply.github.com>
2021-09-11 01:18:53 +0900

    Update return.js

--- return.js
+++ return.js
@@ -10,3 +10,23 @@ function sum(a, b, c) {
 
 const total = sum(1, 2, 3) + sum(3, 4, 5);
 console.log(total);
+
+
+
+// 関数式
+// const 定数名 = funcrion(仮引数,仮引数...) {
+//   処理;
+//   処理;
+//   return 返り値;
+// };    ←；いる！
+
+// 呼び出し
+// 定数名(実引数, 実引数...);
+
+
+const sum = function(a,b,c) {
+  return a + b + c;
+};
+
+const total = sum(1, 2, 3) + sum(3, 4 ,5);
+console.log(total);
