b6a9be6b8b8c1bec8175f15df1d4699d172fe41c
Yasuko <89954618+Yasmin027@users.noreply.github.com>
2021-09-11 15:37:32 +0900

    Update hairetsu.js

--- hairetsu.js
+++ hairetsu.js
@@ -11,3 +11,16 @@
   const scores = [80, 90, 40];
   console.log(scores);
 }
+
+
+{
+  const scores = [80, 90, 40];
+  console.log(scores[1]);     //←90
+
+  scores[2] = 44;        //←40を44に変えたい時
+
+
+  // scores = 10;           ←const(定数)を使って配列全体の変更はできないが、上のように一部の変更は可能
+
+  console.log(scores.length);  //←3（scoresに入っている要素の数)
+}
