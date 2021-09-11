dc7a913490c18144b1a409fc9e9d0668ce6e0dd6
Yasuko <89954618+Yasmin027@users.noreply.github.com>
2021-09-11 21:31:43 +0900

    Create object_2.js

---
+++ object_2.js
@@ -0,0 +1,14 @@
+// 文字列の文字数の取得
+
+{const str = 'hello';
+
+console.log(str.length);  //5
+
+//部分文字列の取得
+//str.substring(開始位置、終了位置);
+console.log(str.substring(2, 4));  //ll
+
+// 個々の文字を取得
+console.log(str[1]); //e
+// ＊配列のように値を指定したりforEachを使うことはできない
+}
